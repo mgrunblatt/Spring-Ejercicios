068288ad199e2a0d1cb267c6a9c9f815f1d6fffa
mgrunblatt <mariano.grunblatt@mercadolibre.com>
2021-03-26 14:02:34 -0300

    Spring Ejercicio 1. Numeros Romanos

---
+++ demo/src/main/java/com/example/demo/Controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.Controllers;
+
+import com.example.demo.Service.RomanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class UserController {
+
+    private final AtomicLong id= new AtomicLong();
+
+    @GetMapping("/convertir")
+    public RomanService numeroRomano(@RequestParam(value = "numero",defaultValue = "numero")int numero){
+
+        RomanService romanService = new RomanService(numero,RomanService.convertirANumerosRomanos(numero));
+
+        return romanService;
+    }
+
+}
